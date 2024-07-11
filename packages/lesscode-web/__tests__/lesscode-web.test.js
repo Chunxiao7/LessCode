a743cce10729c50976efe1af51dd87d0c7298748
leoddeng <3233891353@qq.com>
2024-07-11 20:04:11 +0800

    重构项目结构

---
+++ packages/lesscode-web/__tests__/lesscode-web.test.js
@@ -0,0 +1,7 @@
+'use strict';
+
+const lesscodeWeb = require('..');
+const assert = require('assert').strict;
+
+assert.strictEqual(lesscodeWeb(), 'Hello from lesscodeWeb');
+console.info('lesscodeWeb tests passed');
---
+++ packages/lesscode-web/lib/lesscode-web.js
@@ -0,0 +1,7 @@
+'use strict';
+
+module.exports = lesscodeWeb;
+
+function lesscodeWeb() {
+  return 'Hello from lesscodeWeb';
+}
