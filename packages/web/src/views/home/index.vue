c5272b0fa033affe1e11044e2f23fc1a90f2a900
leoddeng <3233891353@qq.com>
2024-07-29 19:17:39 +0800

    feat: 添加在线编程组件

--- packages/web/src/views/home/index.vue
+++ packages/web/src/views/home/index.vue
@@ -1,7 +1,13 @@
-<script lang="ts" setup></script>
+<script lang="ts" setup>
+import OnlineEditor from "@/components/OnlineEditor/index.vue";
+</script>
 <template>
-  <div>
-    <Button>Click Me</Button>
+  <div class="home-page">
+    <OnlineEditor></OnlineEditor>
   </div>
 </template>
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.home-page {
+  height: 100vh;
+}
+</style>
